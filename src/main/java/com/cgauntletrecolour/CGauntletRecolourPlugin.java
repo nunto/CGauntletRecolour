24010b691f1201ec70ed05f8b777aa06828969ad
Michael <michael.panunto@gmail.com>
2022-10-18 16:51:05 -0400

    Refactoring

--- src/main/java/com/cgauntletrecolour/CGauntletRecolourPlugin.java
+++ src/main/java/com/cgauntletrecolour/CGauntletRecolourPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.cgauntletrecolour;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,16 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Corrupted Gauntlet Recolour",
+	description = "Changes the Corrupted Gauntlet to use the regular Gauntlet's blue theme"
 )
-public class ExamplePlugin extends Plugin
+public class CGauntletRecolourPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private CGauntletRecolourConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +47,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	CGauntletRecolourConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(CGauntletRecolourConfig.class);
 	}
 }
--- src/test/java/com/cgauntletrecolour/CGauntletRecolourPluginTest.java
+++ src/test/java/com/cgauntletrecolour/CGauntletRecolourPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.cgauntletrecolour;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class CGauntletRecolourPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(CGauntletRecolourPlugin.class);
 		RuneLite.main(args);
 	}
 }
